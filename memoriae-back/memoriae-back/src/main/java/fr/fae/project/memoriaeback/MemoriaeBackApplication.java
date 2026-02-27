42517d7c3315b7cc4b56a23b0677222031e3d4ef
Faergoth <164617566+Faergoth@users.noreply.github.com>
2026-02-27 18:56:24 +0100

    00002 - Init Spring Boot project

---
+++ memoriae-back/memoriae-back/src/main/java/fr/fae/project/memoriaeback/MemoriaeBackApplication.java
@@ -0,0 +1,13 @@
+package fr.fae.project.memoriaeback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoriaeBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoriaeBackApplication.class, args);
+	}
+
+}
---
+++ memoriae-back/memoriae-back/src/test/java/fr/fae/project/memoriaeback/MemoriaeBackApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.fae.project.memoriaeback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoriaeBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
