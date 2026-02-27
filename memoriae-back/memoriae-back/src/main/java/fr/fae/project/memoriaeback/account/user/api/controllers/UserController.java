2e2ead971a55c898a167ffff5080ff90ff5bd8cb
Faergoth <164617566+Faergoth@users.noreply.github.com>
2026-02-27 19:41:30 +0100

    00005 - Expose GET /api/users

--- memoriae-back/memoriae-back/src/main/java/fr/fae/project/memoriaeback/account/user/api/controllers/UserController.java
+++ memoriae-back/memoriae-back/src/main/java/fr/fae/project/memoriaeback/account/user/api/controllers/UserController.java
@@ -1,6 +1,8 @@
 package fr.fae.project.memoriaeback.account.user.api.controllers;
 
 import fr.fae.project.memoriaeback.account.user.domain.models.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -8,10 +10,16 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
+@RequestMapping("/api/users")
 public class UserController {
     private List<User> users = new ArrayList<>(List.of(
             new User(UUID.randomUUID(), "Alice", "alice@memoriae.com", "password"),
             new User(UUID.randomUUID(), "Bob", "bob@memoriae.com", "password"),
             new User(UUID.randomUUID(), "Charlie", "charlie@memoriae.com", "password")
     ));
+
+    @GetMapping
+    public List<User> findAll() {
+        return users;
+    }
 }
