0bb354b7396e1f998ea623634df3128d15e75294
pboujonnier2024 <p-boujonnier@outlook.fr>
2026-03-02 11:24:52 +0100

    00029 - Create ApiResponse<T>

---
+++ memoriae-back/src/main/java/fr/fae/project/memoriaeback/account/user/api/common/ApiResponse.java
@@ -0,0 +1,31 @@
+package fr.fae.project.memoriaeback.account.user.api.common;
+
+public class ApiResponse<T> {
+
+    // Attributes
+    public String message;
+    public T data;
+
+    // Constructors
+    public ApiResponse() {
+    }
+    public ApiResponse(String message, T data) {
+        this.message = message;
+        this.data = data;
+    }
+
+    // Getters & Setters
+    public String getMessage() {
+        return message;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getData() {
+        return data;
+    }
+    public void setData(T data) {
+        this.data = data;
+    }
+}
