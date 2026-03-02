7fb0c486991496bc92c06176e0d6ad0b04949eff
pboujonnier2024 <p-boujonnier@outlook.fr>
2026-03-02 13:25:14 +0100

    00032 - Create UserUpdateRequestDTO

---
+++ memoriae-back/src/main/java/fr/fae/project/memoriaeback/account/user/api/dtos/requests/UserUpdateRequest.java
@@ -0,0 +1,7 @@
+package fr.fae.project.memoriaeback.account.user.api.dtos.requests;
+
+public class UserUpdateRequest {
+    private String pseudo;
+    private String email;
+    private String password;
+}
