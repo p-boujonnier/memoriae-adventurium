1fe3808b9caea216f81f8f96f7df1bd9cfc92b08
pboujonnier2024 <p-boujonnier@outlook.fr>
2026-03-04 16:08:23 +0100

    00064 - Gérer MethodArgumentNotValidException dans le handler

--- memoriae-back/src/main/java/fr/fae/project/memoriaeback/common/exceptions/GlobalExceptionHandler.java
+++ memoriae-back/src/main/java/fr/fae/project/memoriaeback/common/exceptions/GlobalExceptionHandler.java
@@ -1,8 +1,36 @@
 package fr.fae.project.memoriaeback.common.exceptions;
+
+import fr.fae.project.memoriaeback.account.user.application.common.ServiceResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.Objects;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ServiceResponse<Void>> handleValidationException(
+            MethodArgumentNotValidException ex) {
+        String rawMessage = ex
+                .getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> Objects.requireNonNullElse(
+                        error.getDefaultMessage(),
+                        "2204:Missing required field"))
+                .findFirst()
+                .orElse("2204:Missing required field");
+
+        String[] parts = rawMessage.split(":", 2);
+        String code = parts[0];
+        String message = parts.length > 1 ? parts[1] : rawMessage;
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ServiceResponse<>(code, message, null));
+    }
 }
