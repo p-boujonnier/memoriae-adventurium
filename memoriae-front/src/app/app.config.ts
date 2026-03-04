b740575a9667de77b0f0339742ea66bf74c9b6b0
pboujonnier2024 <p-boujonnier@outlook.fr>
2026-03-04 16:31:19 +0100

    00066 - Create HttpErrorInterceptor

--- memoriae-front/src/app/app.config.ts
+++ memoriae-front/src/app/app.config.ts
@@ -1,11 +1,12 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { httpErrorInterceptor } from './common/interceptors/http-error.interceptor';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(withInterceptors([httpErrorInterceptor]))
   ]
 };
