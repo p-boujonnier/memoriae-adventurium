21042cdb8cced32b5dc6f44879b70afcddc4c0a1
pboujonnier2024 <p-boujonnier@outlook.fr>
2026-03-04 11:35:17 +0100

    00055 - Integrate Navbar and Footer into app layout

--- memoriae-front/src/app/app.ts
+++ memoriae-front/src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './common/components/navbar/navbar.component';
+import { FooterComponent } from './common/components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {}
