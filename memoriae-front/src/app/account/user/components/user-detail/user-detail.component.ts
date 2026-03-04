ddadce2521e8b0fae008af3c35c4c878364aaf61
pboujonnier2024 <p-boujonnier@outlook.fr>
2026-03-04 09:08:54 +0100

    00043 - Create UserDetailComponent

---
+++ memoriae-front/src/app/account/user/components/user-detail/user-detail.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { UserResponse } from '../../models/dto/user-response.dto';
+import { UserService } from '../../services/user.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-user-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-detail.component.html',
+})
+export class UserDetailComponent implements OnInit{
+  user: UserResponse | null = null;
+
+  constructor(
+    private userService: UserService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    const userId = this.route.snapshot.paramMap.get('id');
+    if (userId) {
+      this.userService.findById(userId).subscribe((data) => {
+        this.user = data;
+      });
+    }
+  }
+
+  onEdit(): void {
+    this.router.navigate(['/users', this.user?.id, 'edit']);
+  }
+
+  onDelete(): void {
+    this.router.navigate(['/users']);
+  }
+}
