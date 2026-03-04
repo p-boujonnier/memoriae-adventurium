656889ee67761992d4127f179d1366e8bace0a64
pboujonnier2024 <p-boujonnier@outlook.fr>
2026-03-04 15:41:20 +0100

    00061 - Annotate UserUpdateRequest with validation constraints

--- memoriae-back/src/main/java/fr/fae/project/memoriaeback/account/user/api/dtos/requests/UserUpdateRequest.java
+++ memoriae-back/src/main/java/fr/fae/project/memoriaeback/account/user/api/dtos/requests/UserUpdateRequest.java
@@ -1,26 +1,37 @@
 package fr.fae.project.memoriaeback.account.user.api.dtos.requests;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 import java.util.UUID;
 
 public class UserUpdateRequest {
 
+    // Attributes
     private UUID id; // TODO: remove when auth is in place
+
+    @NotBlank(message = "2204:Missing required field")
+    @Size(min = 3, message = "2203:Invalid username format")
     private String pseudo;
+
+    @NotBlank(message = "2204:Missing required field")
+    @Email(message = "2200:Invalid email format")
     private String email; // TODO: separate endpoint with confirmation flow
 
+    // Constructors
     public UserUpdateRequest() {
     }
-
     public UserUpdateRequest(UUID id, String pseudo, String email) {
-        this.id = id;
-        this.pseudo = pseudo;
-        this.email = email;
+        this.setId(id);
+        this.setPseudo(pseudo);
+        this.setEmail(email);
     }
 
+    // Getters & Setters
     public UUID getId() {
         return id;
     }
-
     public void setId(UUID id) {
         this.id = id;
     }
@@ -28,7 +39,6 @@ public class UserUpdateRequest {
     public String getPseudo() {
         return pseudo;
     }
-
     public void setPseudo(String pseudo) {
         this.pseudo = pseudo;
     }
@@ -36,7 +46,6 @@ public class UserUpdateRequest {
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
