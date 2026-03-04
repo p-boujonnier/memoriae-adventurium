0f6192479f40a96adcb603855bde1e2750aa8339
pboujonnier2024 <p-boujonnier@outlook.fr>
2026-03-04 09:14:13 +0100

    00044 - Configure routing for User CRUD

--- memoriae-front/src/app/app.routes.ts
+++ memoriae-front/src/app/app.routes.ts
@@ -1,6 +1,12 @@
 import { Routes } from '@angular/router';
 import { UserListComponent } from './account/user/components/user-list/user-list.component';
+import { UserFormComponent } from './account/user/components/user-form/user-form.component';
+import { UserDetailComponent } from './account/user/components/user-detail/user-detail.component';
 
 export const routes: Routes = [
-  { path: '', component: UserListComponent }
+  { path: '', redirectTo: 'users', pathMatch: 'full' },
+  { path: 'users', component: UserListComponent},
+  { path: 'users/new', component: UserFormComponent},
+  { path: 'users/:id', component: UserDetailComponent},
+  { path: 'users/:id/edit', component: UserFormComponent}
 ];
