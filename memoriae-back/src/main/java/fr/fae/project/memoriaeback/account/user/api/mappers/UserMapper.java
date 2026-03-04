d1c9f6be0547f7bb627f8086f49f8bce4424ae27
pboujonnier2024 <p-boujonnier@outlook.fr>
2026-03-04 09:40:54 +0100

    00048 - Adapt UserMapper for UserUpdateRequestDTO

--- memoriae-back/src/main/java/fr/fae/project/memoriaeback/account/user/api/mappers/UserMapper.java
+++ memoriae-back/src/main/java/fr/fae/project/memoriaeback/account/user/api/mappers/UserMapper.java
@@ -1,13 +1,15 @@
 package fr.fae.project.memoriaeback.account.user.api.mappers;
 
 import fr.fae.project.memoriaeback.account.user.api.dtos.requests.UserCreateRequest;
+import fr.fae.project.memoriaeback.account.user.api.dtos.requests.UserUpdateRequest;
 import fr.fae.project.memoriaeback.account.user.api.dtos.responses.UserPublicResponse;
 import fr.fae.project.memoriaeback.account.user.domain.models.User;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
-    User toUser(UserCreateRequest userCreateRequest);
+    User toUserFromCreate(UserCreateRequest userCreateRequest);
+    User toUserFromUpdate(UserUpdateRequest userUpdateRequest);
 
     UserPublicResponse toUserResponse(User user);
 }
