35dae142af7e5472ccf94b9ee7aa39e96c25d676
pboujonnier2024 <p-boujonnier@outlook.fr>
2026-03-04 16:08:01 +0100

    00063 - Create GlobalExceptionHandler for validation errors

---
+++ memoriae-back/src/main/java/fr/fae/project/memoriaeback/common/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,8 @@
+package fr.fae.project.memoriaeback.common.exceptions;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+}
