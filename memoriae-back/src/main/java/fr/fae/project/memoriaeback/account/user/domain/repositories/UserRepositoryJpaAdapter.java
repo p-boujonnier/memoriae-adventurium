55d6fa82f97178985ae8b590430d78fb594457a3
pboujonnier2024 <p-boujonnier@outlook.fr>
2026-03-05 16:28:20 +0100

    00072 - Create UserRepositoryJpaAdapter

---
+++ memoriae-back/src/main/java/fr/fae/project/memoriaeback/account/user/domain/repositories/UserRepositoryJpaAdapter.java
@@ -0,0 +1,55 @@
+package fr.fae.project.memoriaeback.account.user.domain.repositories;
+
+import fr.fae.project.memoriaeback.account.user.domain.models.User;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Profile("jpa")
+@Repository
+public class UserRepositoryJpaAdapter implements IUserRepository {
+    // Injected dependencies
+    private final UserJpaRepository userJpaRepository;
+
+    // Constructors
+    public UserRepositoryJpaAdapter(UserJpaRepository userJpaRepository) {
+        this.userJpaRepository = userJpaRepository;
+    }
+
+    // Methods
+    @Override
+    public Optional<User> findById(UUID id) {
+        return userJpaRepository.findById(id);
+    }
+
+    @Override
+    public List<User> findAll() {
+        return userJpaRepository.findAll();
+    }
+
+    @Override
+    public User save(User user) {
+        return userJpaRepository.save(user);
+    }
+
+    @Override
+    public void deleteById(UUID id) {
+        userJpaRepository.deleteById(id);
+    }
+
+    @Override
+    public boolean existsById(UUID id) {
+        return userJpaRepository.existsById(id);
+    }
+
+    public boolean existsByEmail(String email) {
+        return userJpaRepository.existsByEmail(email);
+    }
+
+    public boolean existsByPseudo(String pseudo) {
+        return userJpaRepository.existsByPseudo(pseudo);
+    }
+}
