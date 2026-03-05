275bb68894ead7ec62e8939710dccc84f42faa83
pboujonnier2024 <p-boujonnier@outlook.fr>
2026-03-05 12:04:36 +0100

    00068e - GlobalExceptionHandler - Simplify validation

--- memoriae-back/src/main/java/fr/fae/project/memoriaeback/common/exceptions/GlobalExceptionHandler.java
+++ memoriae-back/src/main/java/fr/fae/project/memoriaeback/common/exceptions/GlobalExceptionHandler.java
@@ -1,36 +1,36 @@
 package fr.fae.project.memoriaeback.common.exceptions;
 
 import fr.fae.project.memoriaeback.account.user.application.common.ServiceResponse;
+import fr.fae.project.memoriaeback.common.exceptions.dtos.FieldValidationError;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.List;
 import java.util.Objects;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ServiceResponse<Void>> handleValidationException(
+    public ResponseEntity<ServiceResponse<List<FieldValidationError>>> handleValidationException(
             MethodArgumentNotValidException ex) {
-        String rawMessage = ex
+
+        List<FieldValidationError> errors = ex
                 .getBindingResult()
                 .getFieldErrors()
                 .stream()
-                .map(error -> Objects.requireNonNullElse(
-                        error.getDefaultMessage(),
-                        "2204:Missing required field"))
-                .findFirst()
-                .orElse("2204:Missing required field");
-
-        String[] parts = rawMessage.split(":", 2);
-        String code = parts[0];
-        String message = parts.length > 1 ? parts[1] : rawMessage;
+                .map(e -> new FieldValidationError(
+                        e.getField(),
+                        Objects.requireNonNullElse(e.getDefaultMessage(), "Invalid field")
+                ))
+                .toList();
 
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
-                .body(new ServiceResponse<>(code, message, null));
+                .body(new ServiceResponse<>("2200", "Validation error", errors));
     }
 }
