7f6a61187809f074342a466338ad164df8538185
pboujonnier2024 <p-boujonnier@outlook.fr>
2026-03-06 12:08:42 +0100

    00089 - Create LoginRequest DTO

---
+++ memoriae-back/src/main/java/fr/fae/project/memoriaeback/account/auth/api/dtos/LoginRequest.java
@@ -0,0 +1,50 @@
+package fr.fae.project.memoriaeback.account.auth.api.dtos;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class LoginRequest {
+
+    // Attributes
+    @NotBlank(message = "Missing required field")
+    @Size(min = 3, message = "Invalid username format")
+    private String identifier;
+
+    @NotBlank(message = "Missing required field")
+    @Size(min = 8, message = "Password too short")
+    private String password;
+
+    private boolean rememberMe;
+
+    // Constructors
+    public LoginRequest() {
+    }
+    public LoginRequest(String identifier, String password, boolean rememberMe) {
+        this.setIdentifier(identifier);
+        this.setPassword(password);
+        this.setRememberMe(rememberMe);
+    }
+
+    // Getters & Setters
+    public String getIdentifier() {
+        return identifier;
+    }
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isRememberMe() {
+        return rememberMe;
+    }
+    public void setRememberMe(boolean rememberMe) {
+        this.rememberMe = rememberMe;
+    }
+}
