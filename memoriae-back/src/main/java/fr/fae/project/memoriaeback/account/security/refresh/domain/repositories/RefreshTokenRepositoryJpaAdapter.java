129cd81f14cce3ccdf770bde484312f0c5d1a0ac
pboujonnier2024 <p-boujonnier@outlook.fr>
2026-03-06 10:23:24 +0100

    00083 - Create RefreshTokenRepositoryJpaAdapter

---
+++ memoriae-back/src/main/java/fr/fae/project/memoriaeback/account/security/refresh/domain/repositories/RefreshTokenRepositoryJpaAdapter.java
@@ -0,0 +1,45 @@
+package fr.fae.project.memoriaeback.account.security.refresh.domain.repositories;
+
+import fr.fae.project.memoriaeback.account.security.refresh.domain.models.RefreshToken;
+import fr.fae.project.memoriaeback.account.user.domain.models.User;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Repository;
+
+import java.time.OffsetDateTime;
+import java.util.Optional;
+
+@Repository
+@Profile("jpa")
+public class RefreshTokenRepositoryJpaAdapter implements IRefreshTokenRepository {
+
+    private final RefreshTokenJpaRepository refreshTokenJpaRepository;
+
+    public RefreshTokenRepositoryJpaAdapter(RefreshTokenJpaRepository refreshTokenJpaRepository) {
+        this.refreshTokenJpaRepository = refreshTokenJpaRepository;
+    }
+
+    @Override
+    public RefreshToken save(RefreshToken refreshToken) {
+        return refreshTokenJpaRepository.save(refreshToken);
+    }
+
+    @Override
+    public Optional<RefreshToken> findByTokenHash(String tokenHash) {
+        return refreshTokenJpaRepository.findByTokenHash(tokenHash);
+    }
+
+    @Override
+    public boolean revokeAllByUser(User user) {
+        return refreshTokenJpaRepository.revokeAllByUser(user);
+    }
+
+    @Override
+    public boolean revokeByTokenHash(String tokenHash) {
+        return refreshTokenJpaRepository.revokeByTokenHash(tokenHash);
+    }
+
+    @Override
+    public void deleteAllByExpiredAtBefore(OffsetDateTime dateTime) {
+        refreshTokenJpaRepository.deleteAllByExpiredAtBefore(dateTime);
+    }
+}
