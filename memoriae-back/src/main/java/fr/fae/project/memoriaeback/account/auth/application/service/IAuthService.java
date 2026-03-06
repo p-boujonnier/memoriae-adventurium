f84468be47e64a0ea30eafe5c6d9ebd6152e991a
pboujonnier2024 <p-boujonnier@outlook.fr>
2026-03-06 13:00:00 +0100

    00091 - Create IAuthService interface

---
+++ memoriae-back/src/main/java/fr/fae/project/memoriaeback/account/auth/application/service/IAuthService.java
@@ -0,0 +1,17 @@
+package fr.fae.project.memoriaeback.account.auth.application.service;
+
+import fr.fae.project.memoriaeback.account.auth.api.dtos.requests.LoginRequest;
+import fr.fae.project.memoriaeback.account.auth.api.dtos.requests.RegisterRequest;
+import fr.fae.project.memoriaeback.account.auth.api.dtos.responses.AuthResponse;
+import fr.fae.project.memoriaeback.common.ServiceResponse;
+
+public interface IAuthService {
+
+    ServiceResponse<AuthResponse> register(RegisterRequest request);
+
+    ServiceResponse<AuthResponse> login(LoginRequest request, String device, String ipAddress, String userAgent);
+
+    ServiceResponse<Void> logout(String rawRefreshToken);
+
+    ServiceResponse<AuthResponse> refresh(String rawRefreshToken, String device, String ipAddress, String userAgent);
+}
