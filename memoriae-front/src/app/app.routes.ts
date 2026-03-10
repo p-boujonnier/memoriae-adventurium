42c03b309092e5c9945a7263088388d75b3f97e4
pboujonnier2024 <p-boujonnier@outlook.fr>
2026-03-10 09:42:10 +0100

    00100 - Add authGuard to protected routes

--- memoriae-front/src/app/app.routes.ts
+++ memoriae-front/src/app/app.routes.ts
@@ -4,13 +4,14 @@ import { UserFormComponent } from './account/user/components/user-form/user-form
 import { UserDetailComponent } from './account/user/components/user-detail/user-detail.component';
 import { LoginComponent } from './account/auth/components/login/login.component';
 import { RegisterComponent } from './account/auth/components/register/register.component';
+import { authGuard } from './account/auth/guards/auth.guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'users', pathMatch: 'full' },
-  { path: 'users', component: UserListComponent},
-  { path: 'users/new', component: UserFormComponent},
-  { path: 'users/:id', component: UserDetailComponent},
-  { path: 'users/:id/edit', component: UserFormComponent},
+  { path: 'users', component: UserListComponent, canActivate: [authGuard]},
+  { path: 'users/new', component: UserFormComponent, canActivate: [authGuard]},
+  { path: 'users/:id', component: UserDetailComponent, canActivate: [authGuard]},
+  { path: 'users/:id/edit', component: UserFormComponent, canActivate: [authGuard]},
   { path: 'login', component: LoginComponent},
   { path: 'register', component: RegisterComponent },
 ];
