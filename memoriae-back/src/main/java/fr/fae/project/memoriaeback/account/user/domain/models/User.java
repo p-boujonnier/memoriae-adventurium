582912569e2bf168fef86a6d76671588a2603dce
pboujonnier2024 <p-boujonnier@outlook.fr>
2026-03-11 14:32:28 +0100

    00110 - Add Role enum and roles column to User entity

--- memoriae-back/src/main/java/fr/fae/project/memoriaeback/account/user/domain/models/User.java
+++ memoriae-back/src/main/java/fr/fae/project/memoriaeback/account/user/domain/models/User.java
@@ -1,6 +1,7 @@
 package fr.fae.project.memoriaeback.account.user.domain.models;
 
 import fr.fae.project.memoriaeback.account.security.refresh.domain.models.RefreshToken;
+import fr.fae.project.memoriaeback.account.user.domain.models.enums.Role;
 import jakarta.persistence.*;
 
 import java.util.ArrayList;
@@ -11,6 +12,8 @@ import java.util.UUID;
 @Table(name = "users")
 public class User {
 
+
+    // Attributes
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
@@ -30,13 +33,22 @@ public class User {
             orphanRemoval = true, fetch = FetchType.LAZY)
     private List<RefreshToken> refreshTokens = new ArrayList<>();
 
+    @ElementCollection
+    @Enumerated(EnumType.STRING)
+    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "role")
+    private List<Role> roles;
+
+    // Constructors
     public User() {
+        this.setRoles();
     }
 
     public User(String pseudo, String email, String password) {
         this.setPseudo(pseudo);
         this.setEmail(email);
         this.setPassword(password);
+        this.setRoles();
     }
 
     public User(UUID id, String pseudo, String email, String password) {
@@ -44,6 +56,7 @@ public class User {
         this.setId(id);
     }
 
+    // Getters & Setters
     public void setId(UUID id) {
         this.id = id;
     }
@@ -70,5 +83,18 @@ public class User {
     }
     public String getPassword() {
         return password;
+    }
+
+    public void setRoles() {
+        this.roles = new ArrayList<>(List.of(Role.PLAYER));
+    }
+    public void addRole(Role role) {
+        this.roles.add(role);
+    }
+    public void removeRole(Role role) {
+        this.roles.remove(role);
+    }
+    public List<Role> getRoles() {
+        return roles;
     }
 }
---
+++ memoriae-back/src/main/java/fr/fae/project/memoriaeback/account/user/domain/models/enums/Role.java
@@ -0,0 +1,5 @@
+package fr.fae.project.memoriaeback.account.user.domain.models.enums;
+
+public enum Role {
+    PLAYER, GAME_MASTER, ADMIN
+}
